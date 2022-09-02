7e8489d064046240aa2b2ba4404c6c6a0e954a9e
sahadatpro <sahadatsays@gmail.com>
2022-09-03 04:07:34 +0600

    register all model to admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from store.models import *
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+     prepopulated_fields = {"slug": ("name", )}
+     
+admin.site.register(Customer)
+admin.site.register(Brand)
+admin.site.register(Tag)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
