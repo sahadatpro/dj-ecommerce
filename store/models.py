71a8bded0ea9a2f4cc31560eba0a35ff88d371ff
sahadatpro <sahadatsays@gmail.com>
2022-09-03 03:59:49 +0600

    Write models and migrations

--- store/models.py
+++ store/models.py
@@ -1,6 +1,16 @@
+from email.policy import default
 from django.db import models
 from core.mixins.models import AuthorWithTimeMixin
+from django.contrib.auth.models import User
 
+class Customer(models.Model):
+       user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+       name = models.CharField(max_length=200, null=True)
+       email = models.CharField(max_length=200, null=True)
+       
+       def __str__(self) -> str:
+              return self.name
+       
 class Category(AuthorWithTimeMixin):
        name = models.CharField(max_length=200)
        slug = models.SlugField(max_length=200, unique=True)
@@ -51,6 +61,8 @@ class Product(AuthorWithTimeMixin):
        
        description = models.TextField(null=True)
        price  = models.DecimalField(decimal_places=2, max_digits=5)
+       digital = models.BooleanField(default=False, null=True, blank=True)
+       #image = 
        
        
        def __str__(self) -> str:
@@ -58,3 +70,30 @@ class Product(AuthorWithTimeMixin):
        
        class Meta:
               db_table = "products"
+              
+
+class Order(models.Model):
+       customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+       ordered_date = models.DateTimeField(auto_now_add=True)
+       complete = models.BooleanField(default=False, blank=True, null=True)
+       transaction_id = models.CharField(max_length=200, null=True)
+       
+       def __str__(self) -> str:
+              return self.id 
+       
+
+class OrderItem(models.Model):
+       product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
+       order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+       quantity = models.IntegerField(default=0, null=True, blank=True)
+       date_added = models.DateTimeField(auto_now_add=True)
+       
+class ShippingAddress(models.Model):
+       customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+       order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+       address = models.CharField(max_length=100, null=True, blank=True)
+       city = models.CharField(max_length=100, null=True, blank=True)
+       state = models.CharField(max_length=100, null=True, blank=True)
+       zipcode = models.CharField(max_length=100, null=True, blank=True)
+       date_added = models.DateTimeField(auto_now_add=True)
+       
